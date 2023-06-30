43544ba91ef68ea92169ea43d2233fa4a5513f4e
Kirikllz909 <kirikllz909@gmail.com>
2023-06-30 20:03:21 +0300

    Change messageListener for testing

--- src/main/java/discord_bot/command_player/Listeners/MessageListener.java
+++ src/main/java/discord_bot/command_player/Listeners/MessageListener.java
@@ -1,21 +1,26 @@
 package discord_bot.command_player.Listeners;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import discord4j.core.object.entity.Message;
+import discord_bot.command_player.Chat.GetCommandArgs;
+import discord_bot.command_player.Chat.Commands.Command;
+import discord_bot.command_player.Chat.CommandsListeners.CommandListener;
 import reactor.core.publisher.Mono;
 
-public abstract class MessageListener {
-    String msgToSend = "Default message";
+public abstract class MessageListener<T extends Command> {
+
+    @Autowired
+    List<CommandListener<T>> listeners;
 
     public Mono<Void> processCommand(Message eventMessage) {
         if (eventMessage.getAuthor().map(user -> !user.isBot()).orElse(false)) {
-
-            switch (eventMessage.getContent().toLowerCase()) {
-                case "!info":
-                    msgToSend = "Info message";
-                    break;
-            }
-            return Mono.just(eventMessage).flatMap(Message::getChannel)
-                    .flatMap(channel -> channel.createMessage(msgToSend)).then();
+            if (eventMessage.getContent().length() > 0 && eventMessage.getContent().charAt(0) == '!')
+                for (CommandListener<T> l : listeners) {
+                    return l.processCommand(eventMessage, GetCommandArgs.getCommandArgs(eventMessage));
+                }
         }
         return Mono.just(eventMessage).then();
     }
