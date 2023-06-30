648355817e1b68efcf99fb903690090f5d6ac7e9
Kirikllz909 <kirikllz909@gmail.com>
2023-06-30 20:00:46 +0300

    Added class for getting args from message

---
+++ src/main/java/discord_bot/command_player/Chat/GetCommandArgs.java
@@ -0,0 +1,11 @@
+package discord_bot.command_player.Chat;
+
+import java.util.Arrays;
+import discord4j.core.object.entity.Message;
+
+public class GetCommandArgs {
+    public static String[] getCommandArgs(Message msg) {
+        String[] args = msg.getContent().split(" ");
+        return args.length == 1 ? Arrays.copyOfRange(args, 1, args.length) : null;
+    }
+}
