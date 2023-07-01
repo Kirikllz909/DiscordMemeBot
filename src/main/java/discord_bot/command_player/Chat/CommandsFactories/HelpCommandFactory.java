52ad73112b88ab104eecb2f2b571ad691b47d763
Kirikllz909 <kirikllz909@gmail.com>
2023-07-01 21:09:33 +0300

    Added @Service annotation

--- src/main/java/discord_bot/command_player/Chat/CommandsFactories/HelpCommandFactory.java
+++ src/main/java/discord_bot/command_player/Chat/CommandsFactories/HelpCommandFactory.java
@@ -1,7 +1,10 @@
 package discord_bot.command_player.Chat.CommandsFactories;
 
+import org.springframework.stereotype.Service;
+
 import discord_bot.command_player.Chat.Commands.HelpCommand;
 
+@Service
 public class HelpCommandFactory implements CommandFactory<HelpCommand> {
 
     public HelpCommand createCommand() {
--- src/main/java/discord_bot/command_player/Chat/CommandsFactories/JoinCommandFactory.java
+++ src/main/java/discord_bot/command_player/Chat/CommandsFactories/JoinCommandFactory.java
@@ -1,7 +1,10 @@
 package discord_bot.command_player.Chat.CommandsFactories;
 
+import org.springframework.stereotype.Service;
+
 import discord_bot.command_player.Chat.Commands.JoinCommand;
 
+@Service
 public class JoinCommandFactory implements CommandFactory<JoinCommand> {
     public JoinCommand createCommand() {
         return new JoinCommand();
