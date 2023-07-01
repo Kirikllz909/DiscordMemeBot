a9c32776b8e4956d7ba80f85c8af5bb2dbd2452e
Kirikllz909 <kirikllz909@gmail.com>
2023-07-01 19:51:58 +0300

    Changed message for unknown command

--- src/main/java/discord_bot/command_player/Chat/Commands/DefaultCommand.java
+++ src/main/java/discord_bot/command_player/Chat/Commands/DefaultCommand.java
@@ -3,7 +3,7 @@ package discord_bot.command_player.Chat.Commands;
 public class DefaultCommand extends Command {
     public DefaultCommand() {
         super(null,
-                "Unknown command. Check help for command list",
+                "Unknown command. Check !help for command list",
                 null);
     }
 }
