9b3e0589f732ac1e2cb8cbde5e6f78cff092ed9a
Kirikllz909 <kirikllz909@gmail.com>
2023-07-02 02:50:02 +0300

    Created join command processor

---
+++ src/main/java/discord_bot/command_player/Chat/Commands/CommandsListeners/JoinCommandListener.java
@@ -0,0 +1,33 @@
+package discord_bot.command_player.Chat.Commands.CommandsListeners;
+
+import org.springframework.stereotype.Service;
+
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.Message;
+import discord_bot.command_player.BotConfiguration;
+import discord_bot.command_player.Chat.Commands.JoinCommand;
+import reactor.core.publisher.Mono;
+
+@Service
+public class JoinCommandListener implements CommandListener<JoinCommand> {
+
+    @Override
+    public Class<JoinCommand> getCommandType() {
+        return JoinCommand.class;
+    }
+
+    private Member getMember(Message msg) {
+        return msg.getAuthor().get().asMember(msg.getGuildId().get()).block();
+    }
+
+    @Override
+    public Mono<Void> processCommand(Message msg, String[] args) {
+        return Mono.justOrEmpty(getMember(msg))
+                .flatMap(Member::getVoiceState)
+                .flatMap(VoiceState::getChannel)
+                .flatMap(channel -> channel
+                        .join(spec -> spec.setProvider(BotConfiguration.getGuildAudioManager().getProvider())))
+                .then();
+    }
+}
