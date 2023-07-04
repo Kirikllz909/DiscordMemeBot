e4602f8c760be82d071bd7f0e615f75dd0ab6eed
Kirikllz909 <kirikllz909@gmail.com>
2023-07-04 22:05:38 +0300

    created playerManager

---
+++ src/main/java/discord_bot/command_player/Music/PlayerManager.java
@@ -0,0 +1,19 @@
+package discord_bot.command_player.Music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+
+public class PlayerManager {
+    public static final AudioPlayerManager PLAYER_MANAGER;
+
+    static {
+        PLAYER_MANAGER = new DefaultAudioPlayerManager();
+        // This is an optimization strategy that Discord4J can utilize to minimize
+        // allocations
+        PLAYER_MANAGER.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+        AudioSourceManagers.registerRemoteSources(PLAYER_MANAGER);
+        AudioSourceManagers.registerLocalSource(PLAYER_MANAGER);
+    }
+}
