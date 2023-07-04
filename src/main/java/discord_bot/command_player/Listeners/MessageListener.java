b7c4f8ee213730c3105ee436e8fe06e85f8d81f0
Kirikllz909 <kirikllz909@gmail.com>
2023-07-04 22:06:46 +0300

    Set server id for music bot

--- src/main/java/discord_bot/command_player/Listeners/MessageListener.java
+++ src/main/java/discord_bot/command_player/Listeners/MessageListener.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import discord4j.core.object.entity.Message;
+import discord_bot.command_player.BotConfiguration;
 import discord_bot.command_player.Chat.CommandGenerator;
 import discord_bot.command_player.Chat.CommandParser;
 import discord_bot.command_player.Chat.Commands.Command;
-import discord_bot.command_player.Chat.CommandsListeners.CommandListener;
+import discord_bot.command_player.Chat.Commands.CommandsListeners.CommandListener;
 import reactor.core.publisher.Mono;
 
 public abstract class MessageListener<T extends Command> {
@@ -17,9 +18,10 @@ public abstract class MessageListener<T extends Command> {
     List<CommandListener<T>> listeners;
 
     @Autowired
-    CommandGenerator commandGenerator;
+    CommandGenerator<T> commandGenerator;
 
     public Mono<Void> processCommand(Message eventMessage) {
+        BotConfiguration.setGuildAudioManager(eventMessage.getGuildId().get());
         if (eventMessage.getAuthor().map(user -> !user.isBot()).orElse(false)) {
             if (eventMessage.getContent().length() > 0 && eventMessage.getContent().charAt(0) == '!') {
                 for (CommandListener<T> listener : listeners) {
