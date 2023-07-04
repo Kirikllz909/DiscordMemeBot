c2ecf196686b0aa41c53c0e948cf1ff678aa6b15
Kirikllz909 <kirikllz909@gmail.com>
2023-07-04 22:06:04 +0300

    Created guild for all needed classes for music

---
+++ src/main/java/discord_bot/command_player/Music/GuildAudioManager.java
@@ -0,0 +1,50 @@
+package discord_bot.command_player.Music;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.channel.AudioChannel;
+
+public class GuildAudioManager {
+    private static final Map<Snowflake, GuildAudioManager> MANAGERS = new ConcurrentHashMap<>();
+
+    public static GuildAudioManager of(Snowflake id) {
+        return MANAGERS.computeIfAbsent(id, ignored -> new GuildAudioManager());
+    }
+
+    private final AudioPlayer player;
+    private final AudioTrackScheduler scheduler;
+    private final LavaPlayerAudioProvider provider;
+    private AudioChannel voiceChannel;
+
+    private GuildAudioManager() {
+        player = PlayerManager.PLAYER_MANAGER.createPlayer();
+        scheduler = new AudioTrackScheduler(player);
+        provider = new LavaPlayerAudioProvider(player);
+
+        player.addListener(scheduler);
+    }
+
+    public AudioPlayer getPlayer() {
+        return player;
+    }
+
+    public AudioTrackScheduler getScheduler() {
+        return scheduler;
+    }
+
+    public LavaPlayerAudioProvider getProvider() {
+        return provider;
+    }
+
+    public void setVoiceChannel(AudioChannel voiceChannel) {
+        this.voiceChannel = voiceChannel;
+    }
+
+    public AudioChannel getVoiceChannel() {
+        return this.voiceChannel;
+    }
+}
