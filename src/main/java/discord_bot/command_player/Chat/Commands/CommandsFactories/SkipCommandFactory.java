be720989952ead2f5fe57e81855f4611ce4419be
Kirikllz909 <kirikllz909@gmail.com>
2023-07-05 23:46:32 +0300

    Created skip command

---
+++ src/main/java/discord_bot/command_player/Chat/Commands/CommandsFactories/SkipCommandFactory.java
@@ -0,0 +1,14 @@
+package discord_bot.command_player.Chat.Commands.CommandsFactories;
+
+import org.springframework.stereotype.Service;
+
+import discord_bot.command_player.Chat.Commands.SkipCommand;
+
+@Service
+public class SkipCommandFactory implements CommandFactory<SkipCommand> {
+    @Override
+    public SkipCommand createCommand() {
+        return new SkipCommand();
+    }
+
+}
---
+++ src/main/java/discord_bot/command_player/Chat/Commands/CommandsListeners/SkipCommandListener.java
@@ -0,0 +1,30 @@
+package discord_bot.command_player.Chat.Commands.CommandsListeners;
+
+import org.springframework.stereotype.Service;
+
+import discord4j.core.object.entity.Message;
+import discord_bot.command_player.BotConfiguration;
+import discord_bot.command_player.Chat.MessagePrinter;
+import discord_bot.command_player.Chat.Commands.SkipCommand;
+import discord_bot.command_player.Music.SkipDetails;
+import reactor.core.publisher.Mono;
+
+@Service
+public class SkipCommandListener implements CommandListener<SkipCommand> {
+
+    @Override
+    public Class<SkipCommand> getCommandType() {
+        return SkipCommand.class;
+    }
+
+    @Override
+    public Mono<Void> processCommand(Message message, String[] args) {
+        SkipDetails details = BotConfiguration.getGuildAudioManager().getScheduler().skip();
+        if (details.getIsSkipped())
+            return MessagePrinter.printMessage(message,
+                    "Track: " + details.getSkippedTrack().getInfo().title + " is skipped");
+        else
+            return MessagePrinter.printMessage(message, "Unable to skip track");
+    }
+
+}
