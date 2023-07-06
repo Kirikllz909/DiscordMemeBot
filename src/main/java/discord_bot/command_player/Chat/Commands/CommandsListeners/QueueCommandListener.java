b45f5cd1b942e04498d378b255f12dd64eb85a7f
Kirikllz909 <kirikllz909@gmail.com>
2023-07-06 20:30:25 +0300

    Now queue shows all tracks including currently

--- src/main/java/discord_bot/command_player/Chat/Commands/CommandsListeners/QueueCommandListener.java
+++ src/main/java/discord_bot/command_player/Chat/Commands/CommandsListeners/QueueCommandListener.java
@@ -10,6 +10,7 @@ import discord4j.core.object.entity.Message;
 import discord_bot.command_player.BotConfiguration;
 import discord_bot.command_player.Chat.MessagePrinter;
 import discord_bot.command_player.Chat.Commands.QueueCommand;
+import discord_bot.command_player.Music.AudioTrackScheduler;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -22,15 +23,27 @@ public class QueueCommandListener implements CommandListener<QueueCommand> {
 
     @Override
     public Mono<Void> processCommand(Message message, String[] args) {
-        List<AudioTrack> songsList = BotConfiguration.getGuildAudioManager().getScheduler().getQueue();
-        if (songsList == null) {
-            return MessagePrinter.printMessage(message, "There aren't any songs in the queue");
+        AudioTrackScheduler songsListSheduler = BotConfiguration.getGuildAudioManager().getScheduler();
+        List<AudioTrack> songsList = songsListSheduler.getQueue();
+        AudioTrack currentTrack = songsListSheduler.getCurrentTrack();
+
+        String tracksInfo = "There aren't any songs in the queue";
+
+        if (currentTrack != null) {
+            tracksInfo = "Track #1: " + currentTrack.getInfo().title + "\n";
         }
-        Mono<Void> trackQueueProcess = Mono.just(null).then();
+
+        if (songsList.size() == 0) {
+            return MessagePrinter.printMessage(message, tracksInfo);
+        }
+
+        int k = 2;
         for (AudioTrack track : songsList) {
-            trackQueueProcess.then(MessagePrinter.printMessage(message,
-                    "Currently playing: " + track.getInfo().title + "\t Link: " + track.getInfo().uri));
+            {
+                tracksInfo += "Track #" + k + ": " + track.getInfo().title + "\n";
+                k++;
+            }
         }
-        return trackQueueProcess;
+        return MessagePrinter.printMessage(message, tracksInfo);
     }
 }
