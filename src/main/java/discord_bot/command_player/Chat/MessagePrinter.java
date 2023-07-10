4080836f297aa41b656b5f41a2a9c8b4b41123eb
Kirikllz909 <kirikllz909@gmail.com>
2023-07-10 18:06:41 +0300

    Add func  Print message by provided messageChannel

--- src/main/java/discord_bot/command_player/Chat/MessagePrinter.java
+++ src/main/java/discord_bot/command_player/Chat/MessagePrinter.java
@@ -1,9 +1,15 @@
 package discord_bot.command_player.Chat;
 
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
 import reactor.core.publisher.Mono;
 
 public class MessagePrinter {
+    public static Mono<Void> printMessageInChannel(MessageChannel channel, String message) {
+        return channel.createMessage(message)
+                .then();
+    }
+
     public static Mono<Void> printMessage(Message discordMsg, String message) {
         return Mono.just(discordMsg).flatMap(Message::getChannel)
                 .flatMap(channel -> channel.createMessage(message))
